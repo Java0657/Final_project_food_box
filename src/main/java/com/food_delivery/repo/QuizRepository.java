254f47a925f0a787993da630f438d1b2dba0fb51
unknown <rahul24kumar02@gmail.com>
2022-10-04 19:37:20 +0530

    six

--- src/main/java/com/food_delivery/repo/QuizRepository.java
+++ src/main/java/com/food_delivery/repo/QuizRepository.java
@@ -3,12 +3,13 @@ package com.food_delivery.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.food_delivery.model.exam.Category;
 import com.food_delivery.model.exam.Quiz;
 
 import java.util.List;
-
+@Repository
 public interface QuizRepository extends JpaRepository<Quiz, Long> {
     public List<Quiz> findBycategory(Category category);
 
